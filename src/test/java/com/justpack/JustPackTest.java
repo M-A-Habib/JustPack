14bea417f907d8e221cc4e3cfd42f446df33c4be
M-A-Habib <M-A-Habib>
2023-12-05 00:50:53 +0000

    Initial commit

---
+++ src/test/java/com/justpack/JustPackTest.java
@@ -0,0 +1,21 @@
+package com.justpack;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class JustPackTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
